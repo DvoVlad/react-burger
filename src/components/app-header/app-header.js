a3bdd64c1c02899813c7169dc50f38760149431d
Владислав Дворянинов <v.dvoryaninov@notamedia.ru>
2024-11-12 14:51:07 +0300

    Доработки

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -7,12 +7,12 @@ function AppHeader() {
   return (
     <header className={styles.header}>
       <div className={styles.container + ' pt-4 pb-4'}>
-        <AppHeaderLink className="pt-4 pb-4 pl-5 pr-5" current icon={BurgerIcon}>Конструктор</AppHeaderLink>
-        <AppHeaderLink className="ml-2 pt-4 pb-4 pl-5 pr-5" icon={ListIcon}>Лента заказов</AppHeaderLink>
+        <AppHeaderLink className="pt-4 pb-4 pl-5 pr-5 text text_type_main-default" current icon={BurgerIcon}>Конструктор</AppHeaderLink>
+        <AppHeaderLink className="ml-2 pt-4 pb-4 pl-5 pr-5 text text_type_main-default" icon={ListIcon}>Лента заказов</AppHeaderLink>
         <div className={styles.logoWrapper}>
           <Logo />
         </div>
-        <AppHeaderLink className="pt-4 pb-4 pl-5 pr-5" icon={ProfileIcon}>Личный кабинет</AppHeaderLink>
+        <AppHeaderLink className="pt-4 pb-4 pl-5 pr-5 text text_type_main-default" icon={ProfileIcon}>Личный кабинет</AppHeaderLink>
       </div>
     </header>
   );
