9f54a76f54d9057a6e4db73c9f449c6a0dcaa072
Владислав Дворянинов <v.dvoryaninov@notamedia.ru>
2024-11-12 12:37:23 +0300

    Start structure

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './app-header.module.css';
+
+function AppHeader() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+
+      </div>
+    </header>
+  );
+}
+
+export default AppHeader;
