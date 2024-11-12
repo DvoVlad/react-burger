8cb73eb8b7f3bafc9d0fdae8a3f2d2498d2468e3
Владислав Дворянинов <v.dvoryaninov@notamedia.ru>
2024-11-12 15:21:39 +0300

    Добавил валидацию

--- src/components/app-header/app-header-link/app-header-link.js
+++ src/components/app-header/app-header-link/app-header-link.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './app-header-link.module.css';
+import PropTypes from 'prop-types';
 
 function AppHeaderLink({children, current, className, icon}) {
   let classes = className;
@@ -9,11 +10,19 @@ function AppHeaderLink({children, current, className, icon}) {
   if(current) {
     classes = classes + ' ' + styles.active
   }
+  
   return (
     <button className={styles.link + ' ' + classes}>
       <Icon type={type}/><span className='ml-2'>{children}</span>
     </button>
   );
 }
+
+AppHeaderLink.propTypes = {
+  children: PropTypes.string,
+  current: PropTypes.bool,
+  className: PropTypes.string,
+  icon: PropTypes.elementType
+}; 
 
 export default AppHeaderLink;
