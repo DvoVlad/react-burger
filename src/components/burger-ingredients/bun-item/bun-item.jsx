17d2e6110d416f470627cd96f8882b2ced6034f4
Владислав Дворянинов <v.dvoryaninov@notamedia.ru>
2024-11-13 16:33:55 +0300

    Сделал компонент BurgerIngredients

---
+++ src/components/burger-ingredients/bun-item/bun-item.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components'
+import styles from './bun-item.module.css';
+import PropTypes from 'prop-types';
+
+function BunItem({ item }) {
+  return (
+    <article className={styles.item}>
+      <img src={item.image} alt={item.name} className='ml-4 mr-4' />
+      <p className={styles.price + ' text text_type_digits-default mt-1 mb-1'}><span className='mr-2'>{item.price}</span> <CurrencyIcon type="primary"/></p>
+      <h3 className={styles.name + ' text text_type_main-default'}>{item.name}</h3>
+      <Counter count={1} size="default" />
+    </article>
+  );
+}
+
+const itemsShape = PropTypes.shape({
+  _id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  proteins: PropTypes.number.isRequired,
+  fat: PropTypes.number.isRequired,
+  carbohydrates: PropTypes.number.isRequired,
+  calories: PropTypes.number.isRequired,
+  price: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
+  image_mobile: PropTypes.string.isRequired,
+  image_large: PropTypes.string.isRequired,
+  __v: PropTypes.number.isRequired
+});
+
+BunItem.propTypes = {
+  item: itemsShape
+}
+
+export default BunItem;
--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,12 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './burger-ingredients.module.css';
+import { Tab } from '@ya.praktikum/react-developer-burger-ui-components'
+import PropTypes from 'prop-types';
+import BunItem from './bun-item/bun-item';
+import SauceItem from './sauce-item/sauce-item';
+import MainItem from './main-item/main-item';
 
 function BurgerIngredients({ data }) {
+  const [current, setCurrent] = useState('Булки')
+  const buns = data.filter((item) => item.type === 'bun');
+  const sauce = data.filter((item) => item.type === 'sauce');
+  const main = data.filter((item) => item.type === 'main');
+
   return (
     <section>
-      <h1 className='text text_type_main-large'>Соберите бургер</h1>
+      <h1 className='text text_type_main-large mb-5 mt-10'>Соберите бургер</h1>
+      <div className='mb-10' style={{ display: 'flex' }}>
+        <Tab value="Булки" active={current === 'Булки'} onClick={setCurrent}>
+          Булки
+        </Tab>
+        <Tab value="Соусы" active={current === 'Соусы'} onClick={setCurrent}>
+          Соусы
+        </Tab>
+        <Tab value="Начинки" active={current === 'Начинки'} onClick={setCurrent}>
+          Начинки
+        </Tab>
+      </div>
+      <div className={styles.ingredients}>
+        <h2 className='text text_type_main-medium'>Булки</h2>
+        <ul className={styles.itemList + ' pl-4 pr-2 mt-6 mb-10'}>
+          {buns.map((item => (
+            <li key={item._id}>
+              <BunItem item={item}/>
+            </li>
+          )))}
+        </ul>
+        <h2 className='text text_type_main-medium'>Соусы</h2>
+        <ul className={styles.itemList + ' pl-4 pr-2 mt-6 mb-10'}>
+          {sauce.map((item => (
+            <li key={item._id}>
+              <SauceItem item={item}/>
+            </li>
+          )))}
+        </ul>
+        <h2 className='text text_type_main-medium'>Начинки</h2>
+        <ul className={styles.itemList + ' pl-4 pr-2 mt-6'}>
+          {main.map((item => (
+            <li key={item._id}>
+              <MainItem item={item}/>
+            </li>
+          )))}
+        </ul>
+      </div>
     </section>
   );
+}
+
+const itemsShape = PropTypes.shape({
+  _id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  proteins: PropTypes.number.isRequired,
+  fat: PropTypes.number.isRequired,
+  carbohydrates: PropTypes.number.isRequired,
+  calories: PropTypes.number.isRequired,
+  price: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
+  image_mobile: PropTypes.string.isRequired,
+  image_large: PropTypes.string.isRequired,
+  __v: PropTypes.number.isRequired
+});
+
+BurgerIngredients.propTypes = {
+  data: PropTypes.arrayOf(itemsShape)
 }
 
 export default BurgerIngredients;
