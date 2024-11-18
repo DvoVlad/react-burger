4f0d2574754ee09710bec635ac89b91bf284d1d8
Владислав Дворянинов <v.dvoryaninov@notamedia.ru>
2024-11-18 17:13:08 +0300

    Поправка

--- src/components/burger-ingredients/ingredient-details/ingredient-details.jsx
+++ src/components/burger-ingredients/ingredient-details/ingredient-details.jsx
@@ -9,7 +9,7 @@ function IngredientDetails({item}) {
         <img width="480" height="240" src={item.image_large} alt={item.name} />
       </div>
       <h2 className={styles.title + " text text_type_main-medium mt-4 mb-8"}>{item.name}</h2>
-      <ul className={styles.ingredientInfo}>
+      <ul className={styles.ingredientInfo + " mb-15"}>
         <li className={styles.ingredientItem}>
           <p className="text text_type_main-default text_color_inactive mb-2">Калории,ккал</p>
           <p className="text text_type_digits-default text_color_inactive">{item.calories}</p>
