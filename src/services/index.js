bb818c3c5e5ec3dc16d737539284cd0d27f689e5
Владислав Дворянинов <v.dvoryaninov@notamedia.ru>
2024-12-18 12:43:22 +0300

    Убрал лишнее хранилище

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import ingredientsReducer from './ingredients';
 import ingredientsConstructorReducer from './ingredients-constructor';
-import showedIngredientReducer from './showedIngredient';
 import orderReducer from './order';
 import userReducer from './user';
 
@@ -9,7 +8,6 @@ const rootReducer = configureStore({
   reducer: {
     ingredients: ingredientsReducer,
     ingredientsConstructor: ingredientsConstructorReducer,
-    showedIngredient: showedIngredientReducer,
     myOrder: orderReducer,
     user: userReducer
   },
