5c6322461bf1edc99ebaf25148e0854dce5a1ef4
Владислав Дворянинов <v.dvoryaninov@notamedia.ru>
2024-12-18 15:21:12 +0300

    Доработка

--- src/components/burger-constructor/order-details/order-details.jsx
+++ src/components/burger-constructor/order-details/order-details.jsx
@@ -3,13 +3,13 @@ import markImage from "../../../images/order-done.png"
 import { useSelector } from "react-redux";
 function OrderDetails() {
   const loadingStatus = useSelector((store) => store.myOrder.loadingStatus)
-  const ordenNumber = useSelector((store) => store.myOrder.data?.order.number);
+  const ordenData = useSelector((store) => store.myOrder.data);
   const isError = useSelector((store) => store.myOrder.error);
   return(
     <>
       {loadingStatus === 'loading' ? <p className="text_type_main-medium m-5">Загрузка...</p> :
       <>
-        <p className={styles.orderNumber + " mt-30 text text_type_digits-large"}>{ordenNumber}</p>
+        <p className={styles.orderNumber + " mt-30 text text_type_digits-large"}>{ordenData.order.number}</p>
         <p className={styles.orderText + " mb-15 text text_type_main-medium"}>идентификатор заказа</p>
         <div className={styles.imageWrapper + " mb-15"}>
           <img width="107" height="102" src={markImage} alt="Заказ оформлен" />
