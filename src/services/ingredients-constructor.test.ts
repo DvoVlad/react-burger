6d74c2e4186adfa9576a35d1d410646521757ced
Владислав Дворянинов <v.dvoryaninov@notamedia.ru>
2025-02-11 10:25:07 +0300

    Доработал тест хранилища конструктора

--- src/services/ingredients-constructor.test.ts
+++ src/services/ingredients-constructor.test.ts
@@ -127,6 +127,54 @@ describe('ingredients constructor actions', () => {
     expect(state).toEqual({...initialState, items: [addedMain2, addedMain1]});
   });
   it('resetIgredients', () => {
+    const addedBun = {
+      _id: "643d69a5c3f7b9001cfa093c",
+      name: "Краторная булка N-200i",
+      type: "bun",
+      proteins: 80,
+      fat: 24,
+      carbohydrates: 53,
+      calories: 420,
+      price: 1255,
+      image: "https://code.s3.yandex.net/react/code/bun-02.png",
+      image_mobile: "https://code.s3.yandex.net/react/code/bun-02-mobile.png",
+      image_large: "https://code.s3.yandex.net/react/code/bun-02-large.png",
+      __v: 0
+    };
+    const addedMain1 = {
+      _id: "643d69a5c3f7b9001cfa0943",
+      name: "Соус фирменный Space Sauce",
+      type: "sauce",
+      proteins: 50,
+      fat: 22,
+      carbohydrates: 11,
+      calories: 14,
+      price: 80,
+      image: "https://code.s3.yandex.net/react/code/sauce-04.png",
+      image_mobile: "https://code.s3.yandex.net/react/code/sauce-04-mobile.png",
+      image_large: "https://code.s3.yandex.net/react/code/sauce-04-large.png",
+      __v: 0,
+      uuid: 'sdfdsafadsadgdfghdfgdsf'
+    };
+    const addedMain2 = {
+      _id: "643d69a5c3f7b9001cfa0943",
+      name: "Соус фирменный Space Sauce",
+      type: "sauce",
+      proteins: 50,
+      fat: 22,
+      carbohydrates: 11,
+      calories: 14,
+      price: 80,
+      image: "https://code.s3.yandex.net/react/code/sauce-04.png",
+      image_mobile: "https://code.s3.yandex.net/react/code/sauce-04-mobile.png",
+      image_large: "https://code.s3.yandex.net/react/code/sauce-04-large.png",
+      __v: 0,
+      uuid: 'sdfdsafadsadgdfghdfgmmm'
+    };
+    const initialState = {
+      bun: addedBun,
+      items: [addedMain1, addedMain2]
+    };
     const state = ingredientsConstructorSlice.reducer(initialState, {type: resetIgredients.type});
     expect(state).toEqual({...initialState, items: [], bun: null});
   });
